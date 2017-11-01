bcf79b0f99e290178886f8b133c5dda60b6e64ba
Hamayama <hamay1010@gmail.com>
2017-11-02 02:04:41 +0900

    Refactoring test tool

--- test/test-Gambit.scm
+++ test/test-Gambit.scm
@@ -2,37 +2,9 @@
 ;; srfi-48 format test for Gambit
 ;;
 
-(include "srfi-48.scm")
+(load "~~/lib/syntax-case.scm")
 
-(define ok-flag #t)
-
-(define-macro (expect expected form . compare)
-  `(let* ((same?  (if (null? ',compare) equal? (eval (car ',compare))))
-          (wanted ,expected)
-          (actual ,form))
-     (cond
-      ((same? wanted actual)
-       (format #t "PASSED:  ~s~%" ',form))
-      (else
-       (set! ok-flag #f)
-       (format #t
-               "~%**FAIL: expected: ~s~% got:  ~s~% from: ~s~%"
-               wanted
-               actual
-               ',form)))))
-
-(define (test-section txt) (display (string-append "<TEST-SECTION>: " txt)) (newline))
-
-(define (write-with-shared-structure obj port) (error "write/ss is not supported"))
-
-(define (x->number x)
-  (cond
-   ((number? x) x)
-   ((string? x) (string->number x))
-   (else (error "x->number error"))))
-
-(include "test-0001-Gambit.scm")
-
-(display "== RESULT ==") (newline)
-(display (if ok-flag "OK." "FAILED.")) (newline)
+(load "test-tool.scm")
+(load "srfi-48.scm")
+(load "test-0001-Gambit.scm")
 
--- test/test-Guile.scm
+++ test/test-Guile.scm
@@ -4,38 +4,7 @@
 
 (use-modules (srfi srfi-38)) ; for write-with-shared-structure
 
+(include "test-tool.scm")
 (include "srfi-48.scm")
-
-(define ok-flag #t)
-
-(define-syntax expect
-  (syntax-rules ()
-    ((expect ?expected ?form)
-     (expect ?expected ?form equal?))
-    ((expect ?expected ?form ?compare)
-     (let ((expected ?expected)
-           (actual   ?form))
-       (cond
-        ((?compare expected actual)
-         (format #t "PASSED:  ~w~%" '?form))
-        (else
-         (set! ok-flag #f)
-         (format #t
-                 "~%**FAIL: expected: ~w~% got: ~w~% from: ~w~%"
-                 expected
-                 actual
-                 '?form)))))))
-
-(define (test-section txt) (display (string-append "<TEST-SECTION>: " txt)) (newline))
-
-(define (x->number x)
-  (cond
-   ((number? x) x)
-   ((string? x) (string->number x))
-   (else (error "x->number error"))))
-
 (include "test-0001.scm")
-
-(display "== RESULT ==") (newline)
-(display (if ok-flag "OK." "FAILED.")) (newline)
 
